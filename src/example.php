ba050eda2931cc435fb00a16934f72bb52fd4b77
Valters Sakarnis <valters.sakarnis@gmail.com>
2024-07-10 18:02:50 +0300

    Comment on token

--- src/example.php
+++ src/example.php
@@ -5,6 +5,8 @@ require_once 'vendor/autoload.php';
 use Printful\Cache;
 use Printful\PrintfulApi;
 
+// Token is static and exposed freely for the sake of the test,
+// should be in an .env file that is apart of .gitignore
 $token = 'T4JC7mlBCO3bYq7X3fZFpiqS2Y2Z4yiNi4WAVmAu';
 
 $cache = new Cache('cache');
